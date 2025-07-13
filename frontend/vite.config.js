78464dd5c1b6007059044e89bebde709661716c5
Armin Dervisefendic <armindervisefendic@gmail.com>
2025-07-13 15:34:43 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,8 @@ export default defineConfig({
   plugins: [vue()],
   test: {
     environment: 'jsdom',
-    exclude: ['**/__tests__/e2e/**']
+    exclude: [
+      '**/__tests__/e2e/**',
+      'node_modules/**']
   }
 })
