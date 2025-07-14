04ffec98c72baa62891a2f696dc9b2cf968397fd
Armin Dervisefendic <armindervisefendic@gmail.com>
2025-07-14 06:52:13 +0200

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 app.vue:
 <template>
   <div class="app" :class="{ 'green-theme': isGreenTheme }">
-    <h1>Secret Notes</h1>
+    <h1>Secret Notes - Updated</h1>
 
     <!-- Encryption Container -->
     <div class="encrypt-container">
